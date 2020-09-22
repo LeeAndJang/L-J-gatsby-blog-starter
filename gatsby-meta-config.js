c369ab1096f78e0cbc5b58bb3fab4fd226651da3
Ramsbaby <ms6698@naver.com>
2020-09-23 00:42:54 +0900

    first commit

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `너도 할 수 있는 웹개발\n(with 이정우)`,
   description: `Blog posted about ...`,
   author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  introduction: `자바 웹 개발의 진입장벽이 많이 낮아졌습니다. 아직도 웹개발을 고민하시나요? 인문계인 당신도 웹개발자로 만들어드립니다.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
