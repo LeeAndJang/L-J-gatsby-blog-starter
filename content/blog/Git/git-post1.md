c49f158c7a7d26c48a13ab4b1469be3445908caf
JUNGWOOLEE <ms6698@naver.com>
2020-09-24 13:04:16 +0900

    first commit

---
+++ content/blog/Git/git-post1.md
@@ -0,0 +1,13 @@
+---
+title: git 충돌났을때 강제로 Pull하기
+date: 2020-09-24 12:09:49
+category: Git
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
+```sh
+git fetch --all
+git reset --hard origin/master
+git pull origin master
+```
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `너도 할 수 있는 웹개발\n(with 이정우)`,
+  title: `너도 할 수 있는 웹개발 with 이정우`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `이정우`,
   introduction: `자바 웹 개발의 진입장벽이 많이 낮아졌습니다. 아직도 웹개발을 고민하시나요? 인문계인 당신도 웹개발자로 만들어드립니다.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://ramsbaby.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -15,16 +15,16 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'Ramsbaby/ramsbaby-blog-starter', // Your repository for archive comment
   },
   configs: {
-    countOfInitialPost: 10, // Config your initial count of post
+    countOfInitialPost: 5, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'ramsbaby',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'a45979002', // Add your google analytics tranking ID
 }
