e0bad67854e1319795244b9497d16d9f7045259b
JUNGWOOLEE <ms6698@naver.com>
2020-09-24 17:07:17 +0900

    seo-1 commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,14 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://ramsbaby.netlify.app',
+        sitemap: 'https://ramsbaby.netlify.app/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-feed`,
