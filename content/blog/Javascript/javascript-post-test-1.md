43acde327fd380a613f5699460ae79712107318d
JUNGWOOLEE <ms6698@naver.com>
2020-09-24 13:16:31 +0900

    Clean up unnecessary sources

--- content/blog/Javascript/javascript-post-test-1.md
+++
@@ -1,8 +0,0 @@
----
-title: Javascript post test 1
-date: 2020-09-24 12:09:49
-category: Javascript
-thumbnail: { thumbnailSrc }
-draft: false
----
-
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,9 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
-      Gatsby-starter-bee
+    ©<a href="https://github.com/Ramsbaby">Ramsbaby</a>, Built with{' '}
+    <a href="https://github.com/Ramsbaby/ramsbaby-blog-starter">
+      ramsbaby-blog-starter
     </a>
   </footer>
 )
