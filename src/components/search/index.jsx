a63ae520bb207e2593b7369c81a10942904416ea
Ramsbaby <ms6698@naver.com>
2020-10-03 04:04:19 +0900

    검색기능수정(역슬래시추가)

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Input } from 'mdbreact'
 
 import '@fortawesome/fontawesome-free/css/all.min.css'
@@ -9,14 +9,31 @@ import './index.scss'
 
 export const Search = props => {
   const [input, setInput] = useState('')
+  const nameInput = useRef()
 
   const changeHandler = e => {
+    //역슬래시 제거
+    if (e.target.value.toString().indexOf('\\') != -1) {
+      e.target.value.toString().replace(/\\/gi, '')
+      setInput({
+        input: '',
+      })(
+        (nameInput.current.state.innerValue = nameInput.current.state.innerValue.replace(
+          /\\/gi,
+          ''
+        ))
+      )
+
+      e.preventDefault()
+      return false
+    }
     setInput(e.target.value)
+    props.inputSearchWord(e.target.value)
   }
 
   const onFormSubmit = e => {
     e.preventDefault()
-    props.inputSearchWord(input)
+    // props.inputSearchWord(input)
   }
 
   return (
@@ -26,6 +43,7 @@ export const Search = props => {
         label="검색할 단어를 입력해보세요."
         icon="search"
         onChange={changeHandler}
+        ref={nameInput}
       />
     </form>
   )
