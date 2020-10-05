a5534ffa0abcbc4e29643ea914c762ee9e4ef50e
JUNGWOOLEE <ms6698@naver.com>
2020-10-05 15:43:21 +0900

    메타데이터 수정(타이틀)

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: `너도 할 수 있는 웹개발 with 이정우`,
+  title: `너도 할 수 있는 웹개발 　　with 이정우`,
   description: `Blog posted about ...`,
   author: `이정우`,
   introduction: `자바 웹 개발의 진입장벽이 많이 낮아졌습니다. 아직도 웹개발을 고민하시나요? 한달이면 인문계인 당신도 웹개발자가 될 수 있습니다.`,
@@ -8,7 +8,7 @@ module.exports = {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: ``, // Your Facebook account .
     linkedin: ``, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
