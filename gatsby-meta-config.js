70ec47b72fec4ccffa0ceb045d66a2b2a0f77a22
JUNGWOOLEE <ms6698@naver.com>
2020-10-07 10:23:35 +0900

    tag keyword 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -5,20 +5,16 @@ module.exports = {
   introduction: `자바 웹 개발의 진입장벽이 많이 낮아졌습니다. 아직도 웹개발을 고민하시나요? 한달이면 인문계인 당신도 웹개발자가 될 수 있습니다.`,
   siteUrl: `https://ramsbaby.netlify.app`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account .
-    linkedin: ``, // Your LinkedIn account
+    github: `ramsbaby`, // Your GitHub account
   },
   icon: `content/assets/React.js_logo-512.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`blog`,`ramsbaby`,`gatsby-blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
     utterances: 'Ramsbaby/ramsbaby-blog-starter', // Your repository for archive comment
   },
   configs: {
-    countOfInitialPost: 5, // Config your initial count of post
+    countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
     buyMeACoffeeId: 'ramsbaby',
