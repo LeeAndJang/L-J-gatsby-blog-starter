161723ef690a2fcbc7df97962d8095a2b61fee30
Ramsbaby <ms6698@naver.com>
2020-12-27 15:54:58 +0900

    test

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: `너도 할 수 있는 웹개발 　　with 이정우`,
+  title: `test`,
   description: `Blog posted about ...`,
   author: `이정우`,
   introduction: `자바 웹 개발의 진입장벽이 많이 낮아졌습니다. 아직도 웹개발을 고민하시나요? 한달이면 인문계인 당신도 웹개발자가 될 수 있습니다.`,
@@ -8,7 +8,7 @@ module.exports = {
     github: `ramsbaby`, // Your GitHub account
   },
   icon: `content/assets/React.js_logo-512.png`, // Add your favicon
-  keywords: [`blog`,`ramsbaby`,`gatsby-blog`],
+  keywords: [`blog`, `ramsbaby`, `gatsby-blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
     utterances: 'Ramsbaby/ramsbaby-blog-starter', // Your repository for archive comment
@@ -23,14 +23,12 @@ module.exports = {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
   ga: 'UA-179073418-1', // Add your google analytics tranking ID
-  seo:
-  {
+  seo: {
     resolve: 'gatsby-plugin-robots-txt',
     options: {
       host: 'https://ramsbaby.netlify.app',
       sitemap: 'https://ramsbaby.netlify.app/sitemap.xml',
       policy: [{ userAgent: '*', allow: '/' }],
     },
-  }
-  
+  },
 }
